96b85877a0673a4e434385a2edae5ab184b9d1c0
Ananya <76189053+ananya-singh-baghel@users.noreply.github.com>
2023-06-16 15:40:04 +0530

    init commit

---
+++ promise.js
@@ -0,0 +1,26 @@
+const cart =["shoes","pants","kurta"];
+
+//callback hell
+api.createOrder(cart, function(){
+api.proceedToPayment()
+})
+
+
+//same by using promise
+const promise=createOrder(cart);
+
+promise.then(function(orderID){
+    proceedToPayment(orderID);
+});
+
+
+const GITHUB_API="https://...."
+const user=fetch(GITHUB_API);
+
+//another way of writing promises
+
+createOrder(cart)
+    .then((orderId) => proceedToPayment(orderID))
+    .then((paymentInfo) => showOrderSummary(PaymentInfo))
+    .then((orderId) => proceedToPayment(orderID))
+ ;
