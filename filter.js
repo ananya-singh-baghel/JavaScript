37a50e28e3ad7ae5c1c338ace5ba944c7d256a1e
Ananya <76189053+ananya-singh-baghel@users.noreply.github.com>
2023-06-16 14:42:50 +0530

    init commit

---
+++ filter.js
@@ -0,0 +1,78 @@
+const arr=[5,1,3,2,6];
+function double(x){
+return x*2;
+}
+const output = arr.map(double);
+
+function binary(x){
+	return x.toString(2);
+}
+const output = arr.map(binary);
+
+console.log(output);
+
+// (5) ['101', '1', '11', '10', '110']
+
+const arr=[5,1,3,2,6];
+
+const output= arr.map((x) => x.toString(2));
+console.log(output);
+
+//(5) ['101', '1', '11', '10', '110']
+
+const arr=[5,1,3,2,6];
+
+//filter odd values
+
+function isOdd(x){
+return x%2;
+}
+const output= arr.filter(isOdd);
+console.log(output);
+
+//greater than 4
+const output= arr.filter((x) => x>4);
+console.log(output);
+
+//(2) [5, 6]
+
+
+const arr=[5,1,3,2,6];
+
+//sum or max
+
+function findSum(arr){
+	let sum=0;
+	for(let i=0;i<arr.length; i++){
+		sum =sum +arr[i];
+	}
+	return sum;
+}
+console.log(findSum(arr));
+const output=arr.reduce(function(acc,curr){
+    acc=acc+curr;
+    return acc;
+},0);
+console.log(output);
+
+// 17
+// VM2396:17 17
+function findMax(arr){
+	let max=0;
+	for(let i=0;i<arr.length; i++){
+		if(arr[i]>max){
+                max=arr[i];
+                }
+	}
+	return sum;
+}
+
+const output=arr.reduce(function(acc,curr){
+    if(curr>arr){
+        arr=curr;
+     }
+     return max;
+},0);
+console.log(output);
+
+
